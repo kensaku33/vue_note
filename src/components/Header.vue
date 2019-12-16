b92b6e024c6f5be496345a55e40472ace1786249
Kensaku33 <picow.zoe33@gamil.com>
2019-12-16 23:47:13 +0900

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="fixed top-0 w-full p-3">
+    <div class="flex justify-between">
+      <router-link to="/">
+        <h1 class="text-3xl font-bold">note.</h1>
+      </router-link>
+      <div class="flex">
+        <div class="w-10 h-10 rounded-full bg-black mr-2"></div>
+        <div>
+          <p>Pen Write</p>
+          <p class="-mt-1 text-sm">Log Out</p>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import firebase from 'firebase';
+import {auth} from '@/main';
+export default {
+  methods:{
+    signIn(){
+      const provider = new firebase.auth.GoogleAuthProvider()
+      auth.signInWithPopup(provider)
+      .then((result) => {
+        alert('Hello, ' + result.user.displayName + '!')
+      })
+    },
+    signOut(){
+      if(window.confirm('Are You Sure to Sign Out?')){
+        auth.signout()
+        .then(() => {
+          alert('You Safely Signed Out.')
+          this.$router.push('/')
+          location.reload()
+        })
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,32 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+// 追記しました
+import '@/assets/css/tailwind.css'
+import { firestorePlugin } from 'vuefire'          
+import firebase from 'firebase'     
+import 'firebase/firestore'
+import VueDateFns from "vue-date-fns";
+
+Vue.use(VueDateFns);
+
+// firebase
+Vue.use(firestorePlugin)
+firebase.initializeApp({              
+  apiKey: "AIzaSyDyaBWhd4AbaDBufdPilm8Z2_45rLOh888",
+  authDomain: "vue-note-a7fdf.firebaseapp.com",
+  databaseURL: "https://vue-note-a7fdf.firebaseio.com",
+  projectId: "vue-note-a7fdf",
+  storageBucket: "vue-note-a7fdf.appspot.com",
+  messagingSenderId: "465956740476",
+  appId: "1:465956740476:web:a9ce92dffcbc8b2af4d8f9",
+})
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+
+
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
